6193f0efae09a011b64a6a04b054162a7286c5b5
Van_nya <van114514@outlook.com>
2021-01-31 23:23:08 +0800

    Define a command to show information

--- BullshitGenAPI.py
+++ BullshitGenAPI.py
@@ -5,6 +5,8 @@ from random import random, choice
 from json import load
 from re import sub
 
+from mcdreforged.api.command import *
+from mcdreforged.api.types import *
 from mcdreforged.api.rtext import RText, RTextTranslation, RTextList, RAction
 
 PLUGIN_METADATA = {
@@ -22,19 +24,22 @@ PLUGIN_METADATA = {
 }
 
 DEFAULT_DATA_PATH = 'config/BullshitData.json'
+DEFAULT_TITLE = '§e§l§m狗屁不通文章§r'
+DEFAULT_PREFIX = '!!bullshit'
+DEFAULT_KEY = '§6§l§ktest§r'
 REPO_URL = PLUGIN_METADATA['link']
 
 
-def generate(keys: Union[str, List[str], Set[str]] = '§ktest§r', limit: int = 114, famous_chance: float = 51.4, bosh_chance: float = 191.9, breakline_chance: float = 8.10):
+def generate(keys: Union[str, List[str], Set[str]] = DEFAULT_KEY, limit: int = 114, famous_chance: float = 51.4, bosh_chance: float = 191.9, breakline_chance: float = 8.10):
     if type(keys) == str:
         key_list = [keys]
     else:
         key_list = list(keys)
     with open(file=DEFAULT_DATA_PATH, mode='r') as data_file:
         data = load(data_file)
-    title = RText('[§e§l§m狗屁不通文章§r]').h(f'§lDocument§r: §n{REPO_URL}§r').c(RAction.open_url, REPO_URL)
+    title = RText(f'[{DEFAULT_TITLE}]').h(f'§lDocument§r: §n{REPO_URL}§r').c(RAction.open_url, REPO_URL)
     text = '\n '
-    while len(text) <= limit:
+    while len(text) <= abs(limit):
         ran = random()
         total_chance = abs(famous_chance) + abs(bosh_chance) + abs(breakline_chance)
         if ran <= abs(famous_chance) / total_chance:
@@ -51,4 +56,24 @@ def generate(keys: Union[str, List[str], Set[str]] = '§ktest§r', limit: int = 
         RText(text)
         .h(RTextTranslation('chat.copy.click'))
         .c(RAction.copy_to_clipboard, sub(r'§[0-9a-fk-or]', '', RText(text).to_plain_text().strip()))
+    )
+
+
+def on_load(server: ServerInterface, prev):
+    server.register_help_message(
+        DEFAULT_PREFIX,
+        RText('§b[<字数>]§r §e[<关键词>]§r 生成狗屁不通文章')
+        .h(f'所有参数均为可选, 可填写多个§e关键词§r, 以空格分隔\n填写§e关键词§r之前需填写整数形式的§b字数§r\n若未填写任何参数, 则以{DEFAULT_KEY}为§e关键词§r生成§b200字§r的文章')
+    )
+    server.register_command(
+        Literal(DEFAULT_PREFIX)
+        .runs(lambda src: src.reply(generate(limit=200)))
+        .then(
+            Integer('limit')
+            .runs(lambda src, ctx: src.reply(generate(limit=ctx['limit'])))
+            .then(
+                GreedyText('keys')
+                .runs(lambda src, ctx: src.reply(generate(limit=ctx['limit'], keys=ctx['keys'].split())))
+            )
+        )
     )
